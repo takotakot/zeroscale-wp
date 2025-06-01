81a256fe62d0123b18f382f0114354ba05de71d0
takotakot <takotakot@users.noreply.github.com>
2025-06-01 16:52:04 +0900

    feat: Add variables

--- terraform/environments/prd/variables.tf
+++ terraform/environments/prd/variables.tf
@@ -7,3 +7,18 @@ variable "region" {
   type    = string
   default = ""
 }
+
+variable "gce_zone" {
+  type    = string
+  default = "us-west1-c"
+}
+
+variable "gce_instance_id" {
+  type    = string
+  default = "zero-wp-db"
+}
+
+variable "gce_ip" {
+  type    = string
+  default = "10.138.0.2"
+}
